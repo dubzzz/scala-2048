3a9b39172ff54092d262ada698ba154988d55084
Nicolas DUBIEN <github@dubien.org>
2017-06-12 09:58:05 +0200

    [engine/Grid] add toStreams

--- src/main/scala/engine/Grid.scala
+++ src/main/scala/engine/Grid.scala
@@ -16,6 +16,17 @@ object Grid {
     }
   }
 
+  def toStreams(grid: Array[Array[Int]], dir: Direction): Stream[Stream[Int]] = {
+    val size: Int = grid.size
+    def helper() = dir match {
+      case Left  => Stream.from(0).map(y => Stream.from(0).map(x => grid(y)(x)))
+      case Right => Stream.from(0).map(y => Stream.from(0).map(x => grid(size -y -1)(size -x -1)))
+      case Up    => Stream.from(0).map(y => Stream.from(0).map(x => grid(x)(size -y -1)))
+      case Down  => Stream.from(0).map(y => Stream.from(0).map(x => grid(size -x -1)(y)))
+    }
+    helper().map(_.take(size)).take(size)
+  }
+
   def main() = {
     val s = (
         (1 :: 2 :: 3 :: 4 :: Nil).toStream
--- src/test/scala/engine/GridTest.scala
+++ src/test/scala/engine/GridTest.scala
@@ -5,13 +5,20 @@ import org.scalatest.junit.JUnitSuite
 import org.scalatest.prop.Checkers
 
 class GridTest extends JUnitSuite with Checkers {
-  val s = (
+  val test_stream = (
     (1 :: 2 :: 3 :: 4 :: Nil).toStream
       :: (5 :: 6 :: 7 :: 8 :: Nil).toStream
       :: (9 :: 10 :: 11 :: 12 :: Nil).toStream
       :: (13 :: 14 :: 15 :: 16 :: Nil).toStream
       :: Nil
     ).toStream
+
+  val test_grid: Array[Array[Int]] = Array(
+    Array( 1, 2, 3, 4),
+    Array( 5, 6, 7, 8),
+    Array( 9,10,11,12),
+    Array(13,14,15,16)
+  )
 
   @Test
   def ofLeft() {
@@ -21,7 +28,7 @@ class GridTest extends JUnitSuite with Checkers {
       Array( 9,10,11,12),
       Array(13,14,15,16)
     )
-    assertResult(expected)(Grid.of(s, Left))
+    assertResult(expected)(Grid.of(test_stream, Left))
   }
 
   @Test
@@ -32,7 +39,7 @@ class GridTest extends JUnitSuite with Checkers {
       Array( 8, 7, 6, 5),
       Array( 4, 3, 2, 1)
     )
-    assertResult(expected)(Grid.of(s, Right))
+    assertResult(expected)(Grid.of(test_stream, Right))
   }
 
   @Test
@@ -43,7 +50,7 @@ class GridTest extends JUnitSuite with Checkers {
       Array( 2, 6,10,14),
       Array( 1, 5, 9,13)
     )
-    assertResult(expected)(Grid.of(s, Up))
+    assertResult(expected)(Grid.of(test_stream, Up))
   }
 
   @Test
@@ -54,6 +61,30 @@ class GridTest extends JUnitSuite with Checkers {
       Array(15,11, 7, 3),
       Array(16,12, 8, 4)
     )
-    assertResult(expected)(Grid.of(s, Down))
+    assertResult(expected)(Grid.of(test_stream, Down))
+  }
+
+  @Test
+  def toStreamsLeft() {
+    val expected: List[Int] = 1::2::3::4::5::6::7::8::9::10::11::12::13::14::15::16::Nil
+    assertResult(expected)(Grid.toStreams(test_grid, Left).flatten.toList)
+  }
+
+  @Test
+  def toStreamsRight() {
+    val expected: List[Int] = 16::15::14::13::12::11::10::9::8::7::6::5::4::3::2::1::Nil
+    assertResult(expected)(Grid.toStreams(test_grid, Right).flatten.toList)
+  }
+
+  @Test
+  def toStreamsUp() {
+    val expected: List[Int] = 4::8::12::16::3::7::11::15::2::6::10::14::1::5::9::13::Nil
+    assertResult(expected)(Grid.toStreams(test_grid, Up).flatten.toList)
+  }
+
+  @Test
+  def toStreamsDown() {
+    val expected: List[Int] = 13::9::5::1::14::10::6::2::15::11::7::3::16::12::8::4::Nil
+    assertResult(expected)(Grid.toStreams(test_grid, Down).flatten.toList)
   }
 }
