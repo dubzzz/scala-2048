370f7699d9f671a4ceebc55ab8f64cb79ad7bf3c
Nicolas DUBIEN <github@dubien.org>
2017-06-13 00:00:20 +0200

    [random/MersenneTwister] reduce Mersenne Twister to a single method

--- src/main/scala/utility/random/MersenneTwister.scala
+++ src/main/scala/utility/random/MersenneTwister.scala
@@ -20,13 +20,10 @@ object MersenneTwister {
   val MASK_LOWER = (1 << R) - 1
   val MASK_UPPER = (1 << R)
 
-  def seeded(seed: Int): Stream[Int] = {
-    def loop(prev: Int, id: Int): Stream[Int] =
-      prev #:: loop(F * (prev ^ (prev >> 30)) + (id +1), id +1)
-    loop(seed, 0).take(N)
-  }
-
-  def twisted(s: Stream[Int]): Stream[Int] = {
+  def stream(seed: Int): Stream[Int] = {
+    def seeded(prev: Int, id: Int): Stream[Int] = {
+      prev #:: seeded(F * (prev ^ (prev >> 30)) + (id + 1), id + 1)
+    }
     def twisted_next(cur: Int, next: Int, nextM: Int): Int = {
       val x = (cur & MASK_UPPER) + (next & MASK_LOWER)
       val xA = if ((x & 1) == 1) ((x >> 1) ^ A) else (x >> 1)
@@ -40,23 +37,13 @@ object MersenneTwister {
           next.append(nextValues).drop(N-M),
           nextValues))
     }
-    loop(s, s.tail, s.drop(M)).take(N)
-  }
 
-  def extract(s: Stream[Int]): Stream[Int] = {
-    s.map(y0 => {
+    val s = seeded(seed, 0).take(N)
+    loop(s, s.tail, s.drop(M)).map(y0 => {
       val y1 = y0 ^ (y0 >> U)
       val y2 = y1 ^ ((y1 << S) & B)
       val y3 = y2 ^ ((y2 << T) & C)
       y3 ^ (y3 >> L)
     })
-  }
-
-  def stream(seed: Int): Stream[Int] = {
-    def go(prev: Stream[Int]): Stream[Int] = {
-      val tw = twisted(prev)
-      extract(tw).append(go(tw))
-    }
-    go(seeded(seed))
   }
 }
