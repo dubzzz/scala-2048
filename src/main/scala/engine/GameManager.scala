b9a86ed100584da15c18f490c7d68c60f8a177ee
Nicolas DUBIEN <github@dubien.org>
2017-06-16 10:04:14 +0200

    [engine] undo in GameManager

--- src/main/scala/engine/GameManager.scala
+++ src/main/scala/engine/GameManager.scala
@@ -2,17 +2,24 @@ package engine
 
 import utility.random.RandomGenerator
 
-class GameManager(val state: GameState) {
+class GameManager(val previous_states: List[GameState]) {
+  def state(): GameState = {
+    previous_states.head
+  }
   def next(dir: Direction): Option[GameManager] = {
-    state.next(dir).map(new GameManager(_))
+    state().next(dir).map(next_s => new GameManager(next_s :: previous_states))
+  }
+  def undo(): Option[GameManager] = {
+    if (! previous_states.tail.isEmpty) Some(new GameManager(previous_states.tail))
+    else None
   }
   def newGame(): GameManager = {
-    GameManager.of(state.rng)
+    GameManager.of(previous_states.head.rng)
   }
 }
 
 object GameManager {
   def of(rng: RandomGenerator[Int]): GameManager = {
-    new GameManager(GameState.newGame(rng))
+    new GameManager(GameState.newGame(rng) :: Nil)
   }
 }
--- src/main/scala/env/console/Console.scala
+++ src/main/scala/env/console/Console.scala
@@ -8,20 +8,21 @@ object Console {
     var quitGame = false
     var game = GameManager.of(MersenneTwister.of(System.currentTimeMillis.toInt))
     while (! quitGame) {
-      printf("Current state (%d points): \n\n", game.state.score)
-      Grid.toStreams(game.state.grid).foreach(line_stream => {
+      printf("Current state (%d points): \n\n", game.state().score)
+      Grid.toStreams(game.state().grid).foreach(line_stream => {
         print('\t')
         line_stream.foreach(printf("%d\t", _))
         print('\n')
       })
 
       print('\n')
-      val choice = scala.io.StdIn.readLine("Your choice (left/right/up/down/new/quit): ")
+      val choice = scala.io.StdIn.readLine("Your choice (left/right/up/down/undo/new/quit): ")
       choice match {
         case "left"  => game = game.next(Left).getOrElse(game)
         case "right" => game = game.next(Right).getOrElse(game)
         case "up"    => game = game.next(Up).getOrElse(game)
         case "down"  => game = game.next(Down).getOrElse(game)
+        case "undo"  => game = game.undo().getOrElse(game)
         case "new"   => game = game.newGame()
         case "quit"  => quitGame = true
         case _       => printf("[ERROR] Unknown choice: %s\n", choice)
