909c30f5a8bdc622eb9ccfece3296672228ba10c
Nicolas DUBIEN <github@dubien.org>
2017-06-23 19:46:38 +0200

    [random/UniformDistribution] method build generators out of inputs

--- src/main/scala/utility/random/UniformDistribution.scala
+++ src/main/scala/utility/random/UniformDistribution.scala
@@ -3,45 +3,53 @@ package utility.random
 import scala.annotation.tailrec
 
 object UniformDistribution {
+  
   type RNG = RandomGenerator[Int]
   val MIN_VALUE: Long = -(1L << 31)
   val MAX_VALUE: Long = (1L << 31) -1
   val NUM_VALUES: Long = MAX_VALUE - MIN_VALUE +1
+  
+  type Generator[A] = RNG => (A, RNG)
 
-  def nextInt(rng: RNG): (Int, RNG) = rng.next
+  def nextInt: Generator[Int] = (rng: RNG) => rng.next
 
-  def nextInt(from: Int, to: Int)(rng: RNG): (Int, RNG) = {
+  def nextInt(from: Int, to: Int): Generator[Int] = {
     val diff: Long = to.asInstanceOf[Long] - from.asInstanceOf[Long] +1
     val max_allowed: Long = NUM_VALUES - (NUM_VALUES % diff)
-
+    
     @tailrec def go(r: RNG): (Int, RNG) = {
       val (v, nrng) = r.next
       val vv = v.asInstanceOf[Long] - MIN_VALUE
       if (vv < max_allowed) ((vv % diff + from.asInstanceOf[Long]).asInstanceOf[Int], nrng)
       else go(nrng)
     }
-    go(rng)
+
+    (rng: RNG) => go(rng)
   }
 
-  def nextBoolean(rng: RNG): (Boolean, RNG) = {
-    val (v, nrng) = nextInt(0, 1)(rng)
-    (v == 1, nrng)
-  }
+  def nextBoolean: Generator[Boolean] = 
+    (rng: RNG) => nextInt(0, 1)(rng) match {
+      case (0, nrng) => (false, nrng)
+      case (_, nrng) => ( true, nrng)
+    }
 
-  def nextOf[A](first: A, others: A*)(rng: RNG): (A, RNG) = {
-    val (v, nrng) = nextInt(0, others.size)(rng)
-    if (v == 0) (first, nrng) else (others(v-1), nrng)
-  }
+  def nextOf[A](first: A, others: A*): Generator[A] = 
+    (rng: RNG) => nextInt(0, others.size)(rng) match {
+      case (0, nrng) => (first, nrng)
+      case (n, nrng) => (others(n-1), nrng)
+    }
 
-  def nextFrequency[A](choices: (Int, A)*)(rng: RNG): (A, RNG) = {
+  def nextFrequency[A](choices: (Int, A)*): Option[Generator[A]] = {
     val s = choices.toStream
     val count = s.map(_._1).fold(0)(_ + _)
-    val (v, nrng) = nextInt(0, count -1)(rng)
     
     @tailrec def get(ss: Stream[(Int, A)], idx: Int): A = //ss should never be empty
       if (idx < ss.head._1) ss.head._2
       else get(ss.tail, idx - ss.head._1)
 
-    (get(s, v), nrng)
+    if (count == 0 || s.map(_._1).exists(_ < 0)) None
+    else Some(
+      (rng: RNG) => nextInt(0, count -1)(rng) match {
+        case (n, nrng) => (get(s, n), nrng) })
   }
 }
