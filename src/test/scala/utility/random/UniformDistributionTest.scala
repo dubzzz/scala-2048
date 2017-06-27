1fedb30bc06e1c5e5f10034948407095e8a07a5a
Nicolas DUBIEN <github@dubien.org>
2017-06-27 08:44:44 +0200

    [random/UniformDistribution] add test for frequency against of

--- src/test/scala/utility/random/UniformDistributionTest.scala
+++ src/test/scala/utility/random/UniformDistributionTest.scala
@@ -9,8 +9,11 @@ import org.scalatest.prop.Checkers
 class IncrementalInput(val stream: Stream[Int]) extends RandomGenerator[Int] {
   override def next(): (Int, IncrementalInput) =
     (stream.head, new IncrementalInput(stream.tail))
-  def isEmpty(): Boolean =
-    stream.isEmpty
+  override def equals(o: Any): Boolean = o match {
+    case o: IncrementalInput => stream.head == o.stream.head
+    case _ => false
+  }
+  def isEmpty: Boolean = stream.isEmpty
 }
 
 object IncrementalInput {
@@ -61,7 +64,7 @@ class UniformDistributionTest extends JUnitSuite with Checkers {
       case (from, length, target) => {
         var found = false
         var rng = IncrementalInput.of(0, 2*length +1) //twice the length should always be enough (+1 to avoid length = 0)
-        while (! found && ! rng.isEmpty()) {
+        while (! found && ! rng.isEmpty) {
           try {
             val (out, nrng) = UniformDistribution.inRange(from, from + length)(rng)
             rng = nrng.asInstanceOf[IncrementalInput]
@@ -97,6 +100,37 @@ class UniformDistributionTest extends JUnitSuite with Checkers {
         }
         buckets.fold(0)(Math.max) == buckets.fold(Int.MaxValue)(Math.min)
       }
+    }})
+  }
+
+  @Test
+  def propertyFrequencyEquivalentToFlattenOf() {
+    def arityOf(e: (Int, Int)) = e._1
+    def valueOf(e: (Int, Int)) = e._2
+    
+    def validEntry(e: (Int,Int)) = e._1 >= 0 && e._1 <= 10
+    def validEntries(es: List[(Int,Int)]) = es.foldLeft(0)(_ + arityOf(_)) > 0 && ! es.exists(! validEntry(_))
+    def validInput(in: (Int, List[(Int,Int)])) = validEntries(in._2)
+    
+    val entryGen = for {
+      arity <- Gen.choose(0, 10)
+      value <- arbitrary[Int]
+    } yield (arity, value)
+
+    val inputGen = for {
+      offset <- arbitrary[Int]
+      entries <- Gen.listOf(entryGen) suchThat (validEntries(_))
+    } yield (offset, entries)
+
+    check(Prop.forAll(inputGen) {in: (Int, List[(Int,Int)]) => in match {
+      case (offset, entries) if validInput(in) => {
+          var gen = IncrementalInput.infinite(offset)
+          val flattenEq: List[Int] = entries.flatMap(e => Stream.continually(valueOf(e)).take(arityOf(e)).toList)
+          val withOf = UniformDistribution.of(flattenEq.head, flattenEq.tail:_*)(gen)
+          val withFrequency = UniformDistribution.frequency(entries:_*)(gen)
+          withOf == withFrequency
+      }
+      case _ => true
     }})
   }
 }
