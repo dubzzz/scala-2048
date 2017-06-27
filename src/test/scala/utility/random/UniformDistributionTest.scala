638b85060ac82ea3729eb170de17cd0e7945f310
Nicolas DUBIEN <github@dubien.org>
2017-06-27 08:49:39 +0200

    [random/UniformDistribution] handle bad shrinking
    ScalaCheck does not shrink in the range defined at generator level
    as a consequence, when it has to shrink an input it can produce unexpected
    and unhandled entries.
    
    Two approaches are possible:
    - do not treat those entries, they will cause a MatchError in case of
      unexpected shrink
    - treat them as success: you need to be sure that your generator does not
      produce wrong entries too (at least the check in the property need to be
      lighter than the one in the generator)
    
    A flag can be send to the test using -DHANDLE_UNEXPECTED_INPUT=true (or
    false)

--- src/test/scala/utility/random/UniformDistributionTest.scala
+++ src/test/scala/utility/random/UniformDistributionTest.scala
@@ -37,31 +37,50 @@ object IncrementalInput {
 }
 
 class UniformDistributionTest extends JUnitSuite with Checkers {
+  val PROP_HANDLE_UNEXPECTED_INPUT = System.getProperty("HANDLE_UNEXPECTED_INPUT");
+  val HANDLE_UNEXPECTED_INPUT = PROP_HANDLE_UNEXPECTED_INPUT != null && PROP_HANDLE_UNEXPECTED_INPUT.equalsIgnoreCase("true")
+  
+  val MIN_LENGTH = 0
+  val MAX_LENGTH = 1000
+  def validWithin(min: Int, max: Int)(value: Int) = value >= min && value <= max
+  def validLength(from: Int)(length: Int) =
+      validWithin(MIN_LENGTH, MAX_LENGTH)(length) &&
+      from <= length + from
+
   @Test
   def propertyAlwaysInTheRange() {
+    def validTo(from: Int)(to: Int) = from <= to
+    def validInput(in: (Int, Int)) = validTo(in._1)(in._2)
+
     val inputGen = for {
       from <- arbitrary[Int]
-      to <- arbitrary[Int] suchThat (from <= _)
+      to <- arbitrary[Int] suchThat (validTo(from)(_))
     } yield (from, to)
 
-    check(Prop.forAll(inputGen) {inputGen: (Int, Int) => inputGen match {
-      case (from, to) => {
+    check(Prop.forAll(inputGen) {in: (Int, Int) => in match {
+      case (from, to) if validInput(in) => {
         val (out, _) = UniformDistribution.inRange(from, to)(IncrementalInput.of())
         out >= from && out <= to
       }
+      case _ if (HANDLE_UNEXPECTED_INPUT) => true
     }})
   }
 
   @Test
   def propertyCanGenerateWhateverInTheRange() {
+    def validTarget(from: Int, length: Int) = validWithin(from, from + length)(_)
+    def validInput(in: (Int, Int, Int)) =
+        validLength(in._1)(in._2) &&
+        validTarget(in._1, in._2)(in._3)
+
     val inputGen = for {
       from <- arbitrary[Int]
-      length <- Gen.choose(0, 1000) suchThat (from <= _ + from) //arbitrary[Int] suchThat (from <= _)
+      length <- Gen.choose(0, 1000) suchThat (validLength(from)(_))
       target <- Gen.choose(from, from + length)
     } yield (from, length, target)
 
-    check(Prop.forAll(inputGen) {inputGen: (Int, Int, Int) => inputGen match {
-      case (from, length, target) => {
+    check(Prop.forAll(inputGen) {in: (Int, Int, Int) => in match {
+      case (from, length, target) if validInput(in) => {
         var found = false
         var rng = IncrementalInput.of(0, 2*length +1) //twice the length should always be enough (+1 to avoid length = 0)
         while (! found && ! rng.isEmpty) {
@@ -76,20 +95,28 @@ class UniformDistributionTest extends JUnitSuite with Checkers {
         }
         found
       }
+      case _ if (HANDLE_UNEXPECTED_INPUT) => true
     }})
   }
 
   @Test
   def propertyEvenlyDistributed() {
+    val MIN_NUM = 1
+    val MAX_NUM = 100
+    def validNum = validWithin(MIN_NUM, MAX_NUM)(_)
+    def validInput(in: (Int, Int, Int, Int)) =
+        validLength(in._2)(in._3) &&
+        validNum(in._4)
+
     val inputGen = for {
       offset <- arbitrary[Int]
       from <- arbitrary[Int]
-      length <- Gen.choose(0, 1000) suchThat (from <= _ + from)
-      num <- Gen.choose(1, 100)
+      length <- Gen.choose(MIN_LENGTH, MAX_LENGTH) suchThat (validLength(from)(_))
+      num <- Gen.choose(MIN_NUM, MAX_NUM)
     } yield (offset, from, length, num)
 
-    check(Prop.forAll(inputGen) {inputGen: (Int, Int, Int, Int) => inputGen match {
-      case (offset, from, length, num) => {
+    check(Prop.forAll(inputGen) {in: (Int, Int, Int, Int) => in match {
+      case (offset, from, length, num) if validInput(in) => {
         var gen = IncrementalInput.infinite(offset)
         val numRuns = num * (length +1)
         val buckets: Array[Int] = Array.ofDim[Int](length +1)
@@ -100,6 +127,7 @@ class UniformDistributionTest extends JUnitSuite with Checkers {
         }
         buckets.fold(0)(Math.max) == buckets.fold(Int.MaxValue)(Math.min)
       }
+      case _ if (HANDLE_UNEXPECTED_INPUT) => true
     }})
   }
 
@@ -108,18 +136,20 @@ class UniformDistributionTest extends JUnitSuite with Checkers {
     def arityOf(e: (Int, Int)) = e._1
     def valueOf(e: (Int, Int)) = e._2
     
-    def validEntry(e: (Int,Int)) = e._1 >= 0 && e._1 <= 10
+    val MIN_ARITY = 0
+    val MAX_ARITY = 10
+    def validEntry(e: (Int,Int)) = validWithin(MIN_ARITY, MAX_ARITY)(arityOf(e))
     def validEntries(es: List[(Int,Int)]) = es.foldLeft(0)(_ + arityOf(_)) > 0 && ! es.exists(! validEntry(_))
     def validInput(in: (Int, List[(Int,Int)])) = validEntries(in._2)
     
     val entryGen = for {
-      arity <- Gen.choose(0, 10)
+      arity <- Gen.choose(MIN_ARITY, MAX_ARITY)
       value <- arbitrary[Int]
     } yield (arity, value)
 
     val inputGen = for {
       offset <- arbitrary[Int]
-      entries <- Gen.listOf(entryGen) suchThat (validEntries(_))
+      entries <- Gen.listOf(entryGen) suchThat (validEntries _)
     } yield (offset, entries)
 
     check(Prop.forAll(inputGen) {in: (Int, List[(Int,Int)]) => in match {
@@ -130,7 +160,7 @@ class UniformDistributionTest extends JUnitSuite with Checkers {
           val withFrequency = UniformDistribution.frequency(entries:_*)(gen)
           withOf == withFrequency
       }
-      case _ => true
+      case _ if (HANDLE_UNEXPECTED_INPUT) => true
     }})
   }
 }
