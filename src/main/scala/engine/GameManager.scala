ebc1bea57a9bef9bdd9cbae0fbfd9be3c6daf5fc
Nicolas DUBIEN <github@dubien.org>
2017-09-08 00:24:18 +0200

    store history for future replay

--- src/main/scala/engine/GameManager.scala
+++ src/main/scala/engine/GameManager.scala
@@ -4,7 +4,7 @@ import utility.random.RandomGenerator
 
 import scala.annotation.tailrec
 
-class GameManager[State <: GameState](val builder: RandomGenerator[Int] => State, val prevStates: List[State], val redoStates: List[State]) {
+class GameManager[State <: GameState](val builder: RandomGenerator[Int] => State, val prevHistory: List[(State, Direction)], val nextMoves: List[Direction]) {
   def initialState = {
     @tailrec def go(l: List[State], s: State): State = l match {
       case Nil    => s
@@ -12,26 +12,37 @@ class GameManager[State <: GameState](val builder: RandomGenerator[Int] => State
     }
     go(prevStates.tail, prevStates.head)
   }
+  def prevStates = prevHistory.map(_._1)
   def state = prevStates.head
 
   def next(dir: Direction): Option[GameManager[State]] = {
-    state.next(dir).map(nextS => new GameManager(builder, nextS.asInstanceOf[State] :: prevStates, Nil))
+    state.next(dir).map(nextS => new GameManager(builder, (nextS.asInstanceOf[State], dir) :: prevHistory, Nil))
   }
   def undo(): Option[GameManager[State]] = {
-    if (! prevStates.tail.isEmpty) Some(new GameManager(builder, prevStates.tail, prevStates.head :: redoStates))
+    if (! prevStates.tail.isEmpty) Some(new GameManager(builder, prevHistory.tail, prevHistory.head._2 :: nextMoves))
     else None
   }
   def redo(): Option[GameManager[State]] = {
-    if (! redoStates.isEmpty) Some(new GameManager(builder, redoStates.head :: prevStates, redoStates.tail))
+    if (! nextMoves.isEmpty) next(nextMoves.head)
     else None
   }
   def newGame(): GameManager[State] = {
     GameManager.of(initialState.rng.next()._2, builder)
   }
+
+  def stringify(): String = {
+    prevHistory.map(_._2).map(_ match {
+      case Left  => "L"
+      case Right => "R"
+      case Down  => "D"
+      case Up    => "U"
+    }).foldRight("")(_ + _)
+      .substring(1)
+  }
 }
 
 object GameManager {
   def of[State <: GameState](rng: RandomGenerator[Int], builder: RandomGenerator[Int] => State): GameManager[State] = {
-    new GameManager(builder, builder(rng) :: Nil, Nil)
+    new GameManager(builder, (builder(rng), Left) :: Nil, Nil)
   }
 }
--- src/main/scala/env/web/ScalaJS.scala
+++ src/main/scala/env/web/ScalaJS.scala
@@ -110,12 +110,11 @@ object ScalaJS extends js.JSApp {
 
     var seed = System.currentTimeMillis.toInt
     var numNewGames = 0
-    var history = ""
     var game = GameManager.of(
       MersenneTwister.of(seed),
       State2048.newGame(_, numTiles))
 
-    updateHash(seed, numNewGames, history)
+    updateHash(seed, numNewGames, game.stringify())
     drawUnderlyingGrid(area)
 
     var tiles = emptyTiles(numTiles)
@@ -123,30 +122,18 @@ object ScalaJS extends js.JSApp {
 
     dom.document.getElementById("new-game").addEventListener("click", (e: EventTarget) => {
       numNewGames += 1
-      history = ""
       game = game.newGame()
-      updateHash(seed, numNewGames, history)
+      updateHash(seed, numNewGames, game.stringify())
       tiles = updateTiles(area, tiles, game.state.grid)
     })
     dom.document.getElementById("undo-move").addEventListener("click", (e: EventTarget) => {
-      if (! history.isEmpty) {
-        history = history.substring(0, history.length -1)
-      }
       game = game.undo().getOrElse(game)
-      updateHash(seed, numNewGames, history)
+      updateHash(seed, numNewGames, game.stringify())
       tiles = updateTiles(area, tiles, game.state.grid)
     })
     dom.document.getElementById("redo-move").addEventListener("click", (e: EventTarget) => {
       game = game.redo().getOrElse(game)
-      if (game.next(Up).map(_.state.grid == game.state.grid).getOrElse(false))
-        history += "U"
-      else if (game.next(Down).map(_.state.grid == game.state.grid).getOrElse(false))
-        history += "D"
-      else if (game.next(Left).map(_.state.grid == game.state.grid).getOrElse(false))
-        history += "L"
-      else if (game.next(Right).map(_.state.grid == game.state.grid).getOrElse(false))
-        history += "R"
-      updateHash(seed, numNewGames, history)
+      updateHash(seed, numNewGames, game.stringify())
       tiles = updateTiles(area, tiles, game.state.grid)
     })
 
@@ -158,54 +145,38 @@ object ScalaJS extends js.JSApp {
       val endTouch = (e.changedTouches(0).pageX, e.changedTouches(0).pageY)
       val delta = (endTouch._1 - startTouch._1, endTouch._2 - startTouch._2)
       if (2 * Math.abs(delta._1) < Math.abs(delta._2)) {
-        if (delta._2 > 0) {
-          history += "D"
-          game = game.next(Down).getOrElse(game)
-        }
-        else {
-          history += "U"
-          game = game.next(Up).getOrElse(game)
-        }
-        updateHash(seed, numNewGames, history)
+        if (delta._2 > 0) game = game.next(Down).getOrElse(game)
+        else game = game.next(Up).getOrElse(game)
+        updateHash(seed, numNewGames, game.stringify())
         tiles = updateTiles(area, tiles, game.state.grid)
       }
       else if (2 * Math.abs(delta._2) < Math.abs(delta._1)) {
-        if (delta._1 > 0) {
-          history += "R"
-          game = game.next(Right).getOrElse(game)
-        }
-        else {
-          history += "L"
-          game = game.next(Left).getOrElse(game)
-        }
-        updateHash(seed, numNewGames, history)
+        if (delta._1 > 0) game = game.next(Right).getOrElse(game)
+        else game = game.next(Left).getOrElse(game)
+        updateHash(seed, numNewGames, game.stringify())
         tiles = updateTiles(area, tiles, game.state.grid)
       }
     }, false)
 
     dom.window.onkeydown = {(e: dom.KeyboardEvent) => e.keyCode match {
       case 37 /*left*/ => {
-        history += "L"
         game = game.next(Left).getOrElse(game)
-        updateHash(seed, numNewGames, history)
+        updateHash(seed, numNewGames, game.stringify())
         tiles = updateTiles(area, tiles, game.state.grid)
       }
       case 38 /*up*/ => {
-        history += "U"
         game = game.next(Up).getOrElse(game)
-        updateHash(seed, numNewGames, history)
+        updateHash(seed, numNewGames, game.stringify())
         tiles = updateTiles(area, tiles, game.state.grid)
       }
       case 39 /*right*/ => {
-        history += "R"
         game = game.next(Right).getOrElse(game)
-        updateHash(seed, numNewGames, history)
+        updateHash(seed, numNewGames, game.stringify())
         tiles = updateTiles(area, tiles, game.state.grid)
       }
       case 40 /*down*/ => {
-        history += "D"
         game = game.next(Down).getOrElse(game)
-        updateHash(seed, numNewGames, history)
+        updateHash(seed, numNewGames, game.stringify())
         tiles = updateTiles(area, tiles, game.state.grid)
       }
       case _ => ()}}
