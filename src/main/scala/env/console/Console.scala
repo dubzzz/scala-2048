68990860397bfcf613ba8d900e9dbf7ef8a101e6
Nicolas DUBIEN <github@dubien.org>
2017-09-15 21:38:21 +0200

    add state option to the console

--- src/main/scala/env/console/Console.scala
+++ src/main/scala/env/console/Console.scala
@@ -7,7 +7,9 @@ import utility.random.MersenneTwister
 object Console {
   def main(args: Array[String]): Unit = {
     var quitGame = false
-    var game = GameManager.of(MersenneTwister.of(System.currentTimeMillis.toInt), State2048.newGame(_, 4))
+    var seed = System.currentTimeMillis.toInt
+    var numNewGames = 0
+    var game = GameManager.of(MersenneTwister.of(seed), State2048.newGame(_, 4))
     while (! quitGame) {
       printf("Current state (%d points): \n\n", game.state.score)
       Grid.toStreams(game.state.grid).foreach(line_stream => {
@@ -17,7 +19,7 @@ object Console {
       })
 
       print('\n')
-      val choice = scala.io.StdIn.readLine("Your choice (left/right/up/down/undo/redo/new/solve/quit): ")
+      val choice = scala.io.StdIn.readLine("Your choice (left/right/up/down/undo/redo/new/solve/state/quit): ")
       choice match {
         case "left"  => game = game.next(Left).getOrElse(game)
         case "right" => game = game.next(Right).getOrElse(game)
@@ -25,7 +27,10 @@ object Console {
         case "down"  => game = game.next(Down).getOrElse(game)
         case "undo"  => game = game.undo().getOrElse(game)
         case "redo"  => game = game.redo().getOrElse(game)
-        case "new"   => game = game.newGame()
+        case "new"   => {
+          numNewGames += 1
+          game = game.newGame()
+        }
         case "solve" => {
           try {
             val target = scala.io.StdIn.readLine("Expected score: ").toInt
@@ -33,6 +38,7 @@ object Console {
           }
           catch { case e: Exception => printf("[ERROR] Unexpected exception: %s\n", e) }
         }
+        case "state" => printf(s"#seed=${seed}&id=${numNewGames}&history=${game.stringify()}\n")
         case "quit"  => quitGame = true
         case _       => printf("[ERROR] Unknown choice: %s\n", choice)
       }
