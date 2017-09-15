ba9f12a9ddcc9ac41c004c714b8f438c3f3f3237
Nicolas DUBIEN <github@dubien.org>
2017-09-15 21:34:26 +0200

    tailrec solver

--- src/main/scala/solver/DFSSolver.scala
+++ src/main/scala/solver/DFSSolver.scala
@@ -1,20 +1,30 @@
 package solver
 
-import engine.{Down, GameManager, Grid, Left, Right, State2048, Up}
+import engine.{Direction, Down, GameManager, Grid, Left, Right, State2048, Up}
+
+import scala.annotation.tailrec
 
 object DFSSolver {
   def scoreOf(game: GameManager[State2048]): Int =
     Grid.toStreams(game.state.grid).flatten.max
 
-  def solve(game: GameManager[State2048], target: Int): Option[GameManager[State2048]] = {
-    if (scoreOf(game) >= target) {
-      printf("You've reached the target score, current is: %d\n", scoreOf(game))
+  @tailrec
+  def solveHelper(game: GameManager[State2048], target: Int, steps: List[Option[Direction]]): Option[GameManager[State2048]] = {
+    if (scoreOf(game) >= target)
       Some(game)
+    else if (steps.isEmpty)
+      None
+    else if (steps.head.isEmpty)
+      solveHelper(game.undo().get, target, steps.tail)
+    else {
+      val next = game.next(steps.head.get)
+      if (next.isEmpty)
+        solveHelper(game, target, steps.tail.tail)
+      else
+        solveHelper(next.get, target, Some(Left) :: None :: Some(Down) :: None :: Some(Right) :: None :: Some(Up) :: None :: steps.tail)
     }
-    else (Left :: Down :: Right :: Up :: List.empty).toStream
-        .map(dir => game.next(dir).flatMap(solve(_, target)))
-        .filter(g => ! g.isEmpty)
-        .map(g => g.get)
-        .headOption
   }
+
+  def solve(game: GameManager[State2048], target: Int): Option[GameManager[State2048]] =
+    solveHelper(game, target, Some(Left) :: None :: Some(Down) :: None :: Some(Right) :: None :: Some(Up) :: None :: List.empty)
 }
