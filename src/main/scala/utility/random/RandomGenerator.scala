3424e7a1a90a1b7be4a6440cc13965068c5fd282
Nicolas DUBIEN <github@dubien.org>
2017-09-20 22:31:15 +0200

    RandomGenerator is a trait

--- src/main/scala/utility/random/RandomGenerator.scala
+++ src/main/scala/utility/random/RandomGenerator.scala
@@ -1,8 +1,5 @@
 package utility.random
 
-abstract class RandomGenerator[A] {
-  val MIN_VALUE: Long = -(1L << 31)
-  val MAX_VALUE: Long = (1L << 31) -1
-  val NUM_VALUES: Long = 1L << 32
+trait RandomGenerator[A] {
   def next(): (A, RandomGenerator[A]);
 }
