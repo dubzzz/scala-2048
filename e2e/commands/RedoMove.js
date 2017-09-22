cae8d2362109d7037eb29d16e054e47baca79eb2
Nicolas DUBIEN <github@dubien.org>
2017-09-22 03:57:34 +0200

    [e2e] remove unnecessary parameters

--- e2e/commands/RedoMove.js
+++ e2e/commands/RedoMove.js
@@ -2,7 +2,7 @@
 
 const {clickInMenu, readGrid} = require('../helpers');
 
-function RedoMove(direction) {
+function RedoMove() {
     var self = this;
 
     self.check = async function(driver, model) {
