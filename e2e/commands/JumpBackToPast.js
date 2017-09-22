f1457de3a9d5b8d215beddff6f55c6186f9c49bc
Nicolas DUBIEN <github@dubien.org>
2017-09-22 04:39:35 +0200

    [e2e] fix JumpBackToPast

--- e2e/commands/JumpBackToPast.js
+++ e2e/commands/JumpBackToPast.js
@@ -8,15 +8,17 @@ function JumpBackToPast() {
 
     self.check = async function(driver, model) {
         return model.states
-                .map(items => items.length)
+                .map(items => Object.keys(items).length)
                 .reduce((a,b) => a+b, 0) > 0;
     };
     self.run = async function(driver, model) {
         await driver.get("about:blank");
         var id = 0;
         var flatStates = model.states
-                .map(items =>
-                    Object.keys(items).map(key => new Object({id: id++, items: items, key: key})))
+                .map(items => {
+                    var cid = id++;
+                    return Object.keys(items).map(key => new Object({id: cid, url: items[key].url, key: key}))
+                })
                 .reduce((a,b) => a.concat(b), []);
         var item = flatStates[jsc.random(0, flatStates.length -1)];//TODO: use a reproducible random number generator
         await driver.get(item.url);
